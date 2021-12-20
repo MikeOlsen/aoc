de1cee08d338900e4277eb3d0caf17e318600478
Michael Racette Olsén <8173625+MikeOlsen@users.noreply.github.com>
2021-12-20 15:20:55 +0100

    Day 6 - part 2

--- 2021/advent06.py
+++ 2021/advent06.py
@@ -1,5 +1,7 @@
 import aocd
 
+from tabulate import tabulate
+
 test_data = list = [3, 4, 3, 1, 2]
 prod_data = [int(v) for v in aocd.get_data(day=6, year=2021).split(",")]
 
@@ -7,66 +9,56 @@ INIT_LENGTH = 8
 RESET_LENGTH = 6
 
 
-class Fish:
-    def __init__(self, clock=INIT_LENGTH):
-        self.timer = clock
-
-    def tick(self, rate=1):
-        self.timer -= rate
-
-        if self.timer < 0:
-            self.timer = RESET_LENGTH
-            return Fish()
-
-    def __repr__(self):
-        return str(self.timer)
-
-
 class Swarm:
     def __init__(self, list=[]):
-        self.fishes = []
-        for item in list:
-            self.fishes.append(Fish(item))
+        self.clock = [0] * (INIT_LENGTH + 1)
+        for v in list:
+            self.clock[v + 1] += 1
 
-    def append(self, fish):
-        if fish:
-            self.fishes.append(fish)
+    def tick(self):
+        reset = self.clock[0]
+        self.clock[0] = 0
 
-    def extend(self, children):
-        self.fishes.extend(children.fishes)
+        # Rotate left
+        self.clock = self.clock[1:] + self.clock[:1]
+
+        self.clock[8] += reset
+        self.clock[6] += reset
+
+    def count(self):
+        return sum(self.clock)
 
     def __repr__(self):
-        return ", ".join(str(v) for v in self.fishes)
+        days = ["Clock"]
+        days.extend([str(_) for _ in range(1, INIT_LENGTH + 1)])
+        return tabulate([self.clock], headers=days)
 
 
-def simulate(data, days, verbose=1):
+def simulate(data, days, verbose=False):
     swarm = Swarm(data)
 
-    for i in range(days):
-        children = Swarm()
-        for fish in swarm.fishes:
-            children.append(fish.tick())
-        swarm.extend(children)
+    for i in range(days + 1):
+        swarm.tick()
 
         if verbose == 1:
-            print(f"Day {i+1}:\t {len(swarm.fishes)} -> {len(children.fishes)}")
-        elif verbose >= 2:
-            print(
-                f"Day {i+1}:\t {swarm} ->"
-                f" total: {len(swarm.fishes)}"
-                f" new: {len(children.fishes)}"
-            )
+            print("Day:", i)
+            print(swarm, end="\n\n")
 
-    return len(swarm.fishes)
+    return swarm.count()
 
 
 if __name__ == "__main__":
 
-    print()
-    print("=============== PART 1 ==================")
-    print("[TEST] Total:", simulate(test_data, 18, verbose=2))
+    print("\n=============== PART 1 ==================")
+    print(
+        f"------------------------------------------\n"
+        f"[TEST] Total: {simulate(test_data, 18, verbose=True)}"
+    )
+    print("[TEST] Total:", simulate(test_data, 80, verbose=False))
     print("-----------------------------------------")
-    print("[TEST] Total:", simulate(test_data, 80, verbose=1))
+    print("[PROD] Total:", simulate(prod_data, 80, verbose=False))
     print("-----------------------------------------")
-    print("[PROD] Total:", simulate(prod_data, 80, verbose=1))
+
+    print("\n=============== PART 2 ==================")
+    print(f"[PROD] Total: {simulate(prod_data, 256, verbose=False):,}")
     print("-----------------------------------------")
